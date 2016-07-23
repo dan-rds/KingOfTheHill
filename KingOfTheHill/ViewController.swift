df2b884f7bd9c5af65e185977e8347eb1c7cbc23
Daniel Richards <ddrichar@ucsc.edu>
2016-07-23 15:13:24 -0700

    Map on main screen

--- KingOfTheHill/ViewController.swift
+++ KingOfTheHill/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
  
+    @IBOutlet weak var mapView: MKMapView!
+    
     @IBAction func MenuPressed(_ sender: UIBarButtonItem) {
         toggleSideMenuView()
 
@@ -18,6 +21,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //37.375596, -121.919963
+        var latitude:CLLocationDegrees = 37.375596
+        var longitude:CLLocationDegrees = -121.919963
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+    
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        mapView.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
