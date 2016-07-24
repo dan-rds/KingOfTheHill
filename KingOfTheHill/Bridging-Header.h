aec99177bd065c620ce1428e3c9441a332a84312
Jon Richards <dev@moversandshakers.moni>
2016-07-23 17:59:33 -0700

    Added GeoFire functions

---
+++ KingOfTheHill/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  KingOfTheHill
+//
+//  Created by Jon Richards on 7/23/16.
+//  Copyright © 2016 Daniel Richards. All rights reserved.
+//
+
+#import "GeoFire.h"
--- KingOfTheHill/ViewController.swift
+++ KingOfTheHill/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 import MapKit
+import Firebase
+import FirebaseDatabase
+import CoreLocation
+
+
 
 class ViewController: UIViewController, MKMapViewDelegate {
- 
+    
+    
     @IBOutlet weak var mapView: MKMapView!
+    
     
     @IBAction func MenuPressed(_ sender: UIBarButtonItem) {
         toggleSideMenuView()
@@ -32,11 +39,51 @@ class ViewController: UIViewController, MKMapViewDelegate {
         var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
         var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
         mapView.setRegion(region, animated: true)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupEvents(lat: Double, lon: Double) {
+        
+        if let circleQuery = geoFire?.query(at: center, withRadius: 10.0) {
+
+            let queryHandle = circleQuery.observe(.keyEntered, with: { (key, location) in
+                print("key = \(key) location = \(location)")
+            })
+            print("handle = \(queryHandle)")
+        } else {
+            print("circle query is nil")
+        }
+
+ 
+    }
+    
+    func storeLoc(name: String, lat: Double, lon: Double) {
+        let geofireRef = FIRDatabase.database().reference()
+        let geoFire = GeoFire(firebaseRef: geofireRef)
+
+        geoFire?.setLocation(CLLocation(latitude: lat, longitude: lon), forKey: name)
+    }
+    
+    func getLoc(name: String) {
+        
+        let geofireRef = FIRDatabase.database().reference()
+        let geoFire = GeoFire(firebaseRef: geofireRef)
+
+        geoFire?.getLocationForKey(name, withCallback: { (location, error) in
+            if (error != nil) {
+                print("An error occurred getting the location: \(error?.localizedDescription)")
+            } else if (location != nil) {
+                print("Location for \(name) is [\(location?.coordinate.latitude), \(location?.coordinate.longitude)]")
+            } else {
+                print("GeoFire does not contain a location for \"firebase-hq\"")
+            }
+        })
     }
 
     
